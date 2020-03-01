37d0296149734e990d6a3e02e040acfcbed46f30
SwiftEngineer <harbinzerg@gmail.com>
2020-02-29 16:03:57 -0800

    remove requirements for list resources from dao creation

--- src/ElmData/DAO.elm
+++ src/ElmData/DAO.elm
@@ -1,8 +1,8 @@
-module ElmData.DAO exposing (DAO, createDAO)
+module ElmData.DAO exposing (DAO, createDAO, createAuthenticatedDAO)
 
 {-| DAOs (Data Access Objects) exist to hold all state relevant to making requests to an API.
 
-@docs DAO, createDAO
+@docs DAO, createDAO, createAuthenticatedDAO
 -}
 
 import Json.Decode
@@ -18,33 +18,30 @@ type alias DAO recordType =
     , authConfig : AuthConfig
 
     -- serialization
-    , listDeserialize : Json.Decode.Decoder (List recordType)
     , deserialize : Json.Decode.Decoder recordType
     , serialize : (recordType -> Json.Encode.Value)
     }
 
 {-| Function used to create a DAO
 -}
-createDAO : String -> Json.Decode.Decoder (List recordType) -> Json.Decode.Decoder recordType -> (recordType -> Json.Encode.Value) -> (DAO recordType)
-createDAO apiUrl listDeserializer deserializer serializer =
+createDAO : String -> Json.Decode.Decoder recordType -> (recordType -> Json.Encode.Value) -> (DAO recordType)
+createDAO apiUrl deserializer serializer =
     { apiUrl = apiUrl
     , authConfig = AuthConfig.default
 
     -- serialization
-    , listDeserialize = listDeserializer
     , deserialize = deserializer
     , serialize = serializer
     }
 
 {-| Function used to create a DAO
 -}
-createAuthenticatedDAO : String -> AuthConfig -> Json.Decode.Decoder (List recordType) -> Json.Decode.Decoder recordType -> (recordType -> Json.Encode.Value) -> (DAO recordType)
-createAuthenticatedDAO apiUrl authConfig listDeserializer deserializer serializer =
+createAuthenticatedDAO : String -> AuthConfig -> Json.Decode.Decoder recordType -> (recordType -> Json.Encode.Value) -> (DAO recordType)
+createAuthenticatedDAO apiUrl authConfig deserializer serializer =
     { apiUrl = apiUrl
     , authConfig = authConfig
 
     -- serialization
-    , listDeserialize = listDeserializer
     , deserialize = deserializer
     , serialize = serializer
     }
